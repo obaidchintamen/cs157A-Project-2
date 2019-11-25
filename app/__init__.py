4f51aca67ffec61e365b2f732217059562598441
Obaid <obaidc1@gmail.com>
2019-11-24 19:23:04 -0800

    initial first page with four buttons and add visit ui

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from config import Config
 app = Flask(__name__)
+app.config.from_object(Config)
 
-@app.route("/")
-    def hello():
-        return "Hello World!"
+from app import routes
 
-if __name__ == "__main__":
-    app.run()
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+# if __name__ == "__main__":
+#     app.run()
