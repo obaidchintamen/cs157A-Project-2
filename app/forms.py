7621ce9d94419eccfde6b491f2de1f1581a489f0
skuchibotla <skuchibotla1998@gmail.com>
2019-11-25 18:04:22 -0800

    created initial interview form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,92 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, DateField, BooleanField, TextAreaField, validators
+from wtforms.validators import DataRequired, Required
 
 class AddVisitForm(FlaskForm):
-    visit_ID = StringField('Visit ID', validators=[])
-    patient_ID = StringField('Pateint Number',validators=[])
-    thc_number = StringField('TBH #',validators=[])
-    date = StringField('Date',validators=[])
+    visit_ID = StringField('Visit ID', validators = [])
+    patient_ID = StringField('Pateint Number',validators = [])
+    thc_number = IntegerField('THC #',validators = [])
+    date = DateField('Date',validators = [])
+
+class AddInterviewForm(FlaskForm):
+    visit_ID = StringField('Visit ID', validators = [])
+    date = DateField('Date', validators = [])
+    thc_number = IntegerField('THC #', validators = [])
+    patient_ID = StringField('Patient Number', validators = [])
+    visit_number = IntegerField('Visit No.', validators = [])
+
+class AddInitialInterviewForm(FlaskForm):
+    # Beginning info
+    clinic_number = IntegerField('Clinic #:', validators = [])
+    first_name = StringField('First Name:', validators = [])
+    last_name = StringField('Last Name:', validators = [])
+    dob = DateField('DOB:', validators = [])
+    ssn = IntegerField('SSN:', validators = [])
+    insurance = StringField('Insurance:', validators = [])
+    date = DateField('Date:', validators = [])
+    thc = IntegerField('T&HC:', validators = [])
+    telephone = IntegerField('Telephone:', validators = [])
+    email = StringField('Email:', validators = [])
+
+    ''' TINNITUTS '''
+    re = BooleanField('RE', validators = [])
+    le = BooleanField('LE', validators = [])
+    both = BooleanField('Both', validators = [])
+    head = BooleanField('Head', validators = [])
+    intermittent = BooleanField('Intermittent', validators = [])
+    constant = BooleanField('Constant', validators = [])
+
+    gradual = BooleanField('Gradual', validators = [])
+    sudden = BooleanField('Sudden', validators = [])
+    when = StringField('When', validators = [])
+    fluctuations_volume_yes = BooleanField('Yes', validators = [])
+    fluctuations_volume_no = BooleanField('No', validators = [])
+    description_t_sound = TextAreaField('Description of T sound(s):', validators = [])
+
+    # Activities prevented
+    concentration_t_init = BooleanField('Concentration', validators = [])
+    sleep_t_init = BooleanField('Sleep', validators = [])
+    qra_t_init = BooleanField('QRA', validators = [])
+    work_t_init = BooleanField('Work', validators = [])
+    restaurants_t_init = BooleanField('Restaurants', validators = [])
+    sports_t_init = BooleanField('Sports', validators = [])
+    social_t_init = BooleanField('Social', validators = [])
+    other_t_init = BooleanField('Other', validators = [])
+
+    # % of time when
+    aware_t_init = IntegerField('Aware', validators = [])
+    annoyed_t_init = IntegerField('Annoyed', validators = [])
+    severity_t_init = IntegerField('Severity', validators = [])
+    annoyance_t_init = IntegerField('Annoyance', validators = [])
+    effectonlife_t_init = IntegerField('Effect on Life', validators = [])
+
+    # Bad Days 
+    baddays_yes_t_init = BooleanField('Yes', validators = [])
+    baddays_no_t_init = BooleanField('No', validators = [])
+    baddays_freq_t_init = IntegerField('Frequency', validators = [])
+
+    # Effect of Sound
+    none_t_init = BooleanField('None', validators = [])
+    louder_t_init = BooleanField('Louder', validators = [])
+    softer_t_init = BooleanField('Softer', validators = [])
+    mins_t_init = BooleanField('Mins', validators = [])
+    hours_t_init = BooleanField('Hours', validators = [])
+    days_t_init = BooleanField('Days', validators = [])
+
+    # Ear overprotection
+    overprotection_yes_t_init = BooleanField('Yes', validators = [])
+    overprotection_no_t_init = BooleanField('No', validators = [])
+    overprotection_freq_t_init = IntegerField('Percent of time when', validators = [])    
+    quiet_yes_t_init = BooleanField('Yes', validators = [])
+    quiet_no_t_init = BooleanField('No', validators = []) 
+
+    # Comments
+    comments_spec_t_init = StringField('Any other T specific treatments:', validators = [])
+    problem_t_init = StringField('Why is T a problem?', validators = [])   
+    
+    
+
+
+
+
+    
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_wtf import FlaskForm
 from app.forms import AddVisitForm
+from app.forms import AddInterviewForm
+from app.forms import AddInitialInterviewForm
 from app import app
-
 
 @app.route('/index')
 def index():
-    buttons = {'add_visit':'add visit', 
-    'add_interview':'add interview', 
-    'lookup_visit':'lookup visit',
-    'other':'other'}
+    buttons = {
+        'add_visit':'Add Visit', 
+        'add_interview':'Add Interview', 
+        'lookup_visit':'Lookup Visit',
+        'other':'Other',
+        }
     
-    return render_template('index.html', buttons=buttons)
+    return render_template('index.html', buttons = buttons)
 # buttons.add_visit = 'add visit'
 #buttons.key -> value
 
@@ -19,3 +22,22 @@ def index():
 def add_visit():
     form = AddVisitForm()
     return render_template('add_visit.html', form = form)
+
+@app.route('/add_interview')
+def add_interview():
+    buttons = {
+        'initial_followup':'Initial/Followup',
+        'thi':'THI',
+        'tfi':'TFI',
+        'add_md':'Add MD'
+    }
+    form = AddInterviewForm()
+    return render_template('add_interview.html', form = form, buttons = buttons)
+
+@app.route('/init_interview')
+def init_interview():
+    buttons = {
+        'save':'Save'
+    }
+    form = AddInitialInterviewForm()
+    return render_template('init_interview.html', form = form, buttons = buttons)
