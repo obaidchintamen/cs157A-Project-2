2152463ef42f61c22c5d905144a67708449af59c
Obaid <obaidc1@gmail.com>
2019-11-26 17:15:17 -0800

    edited add_visit.html and routes for get form data

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, IntegerField, SubmitField, DateField, validators
 from wtforms.validators import DataRequired
 
 class AddVisitForm(FlaskForm):
-    visit_ID = StringField('Visit ID', validators=[])
-    patient_ID = StringField('Pateint Number',validators=[])
-    thc_number = StringField('TBH #',validators=[])
-    date = StringField('Date',validators=[])
+    visit_ID = IntegerField('Visit ID', validators=[DataRequired()])
+    patient_ID = IntegerField('Pateint Number',validators=[DataRequired()])
+    thc_number = IntegerField('TBH #',validators=[DataRequired()])
+    date = DateField('Date',validators=[DataRequired()], format = '%Y-%m-%d')
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash
 from flask_wtf import FlaskForm
 from app.forms import AddVisitForm
 from app import app
 
+from flaskext.mysql import MySQL
+mysql = MySQL()
+mysql.init_app(app)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Newssigmabob111!'
+app.config['MYSQL_DATABASE_DB'] = 'projectDB'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+conn = mysql.connect()
 
 @app.route('/index')
 def index():
@@ -15,7 +23,17 @@ def index():
 # buttons.add_visit = 'add visit'
 #buttons.key -> value
 
-@app.route('/add_visit')
+@app.route('/add_visit', methods=['GET', 'POST'])
 def add_visit():
     form = AddVisitForm()
+    cursor = conn.cursor()
+    if form.validate_on_submit():
+        visitId = form.visit_ID.data
+        patientId = form.patient_ID.data
+
+        return 'Submitted values: \n Visit ID : {} \n Patient ID : {}'.format(visitId, patientId)
+    cursor.close()
     return render_template('add_visit.html', form = form)
+
+
+conn.close()
