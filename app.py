72c475fe051f3f5fdb151f273ccf80e4a5dd0694
Obaid <obaidc1@gmail.com>
2019-11-27 19:53:41 -0800

    installed dependency to environmetn

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # Kevin's using python connect to mysql workbench
 
-import mysql.connector
+# import mysql.connector
 
-mydb = mysql.connector.connect(host='localhost', user='root', passwd='aaa123123123')
+# mydb = mysql.connector.connect(host='localhost', user='root', passwd='aaa123123123')
 
-mycursor = mydb.cursor()
+# mycursor = mydb.cursor()
 
-mycursor.execute("show databases")
+# mycursor.execute("show databases")
 
-for i in mycursor:
-    print(i)
+# for i in mycursor:
+#     print(i)
